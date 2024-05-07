56a9dd8e8eb3f0283243e635010e76c69a511fad
zezo773 <zezo77328@gmail.com>
2024-05-07 10:25:30 -0700

    asdf

---
+++ _printf.c
@@ -0,0 +1,78 @@
+#include "main.h"
+
+/**
+ * _printf - The Entry Point
+ *
+ * a function that produces output according to a format.
+ *
+ * Return: intger
+ */
+
+int _printf(const char *format, ...)
+{
+	int car;
+
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			car++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+			{
+				break;
+			}
+			else if (*format == '%')
+			{
+				write(1, format, 1);
+				car++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				write(1, &c, 1);
+				car++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char*);
+				int str_len = 0;
+
+				while (str[str_len] != '\0')
+				{
+					str_len++;
+				}
+				write(1, str, str_len);
+				car += str_len;
+			}
+		}
+
+
+		format++;
+	}
+	va_end(args);
+
+	return (car);
+}
+
+
+
+int main()
+{
+	_printf("ziad\n");
+	_printf("%c\n", 'z');
+	_printf("%s\n", "mena");
+	_printf("%%\n");
+	return (0);
+}
