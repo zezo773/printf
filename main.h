ab8b8222c7881aa91db83033a0ab9ae41c0cd6ab
zezo773 <zezo77328@gmail.com>
2024-05-08 01:27:40 -0700

    asdf

--- main.h
+++ main.h
@@ -5,5 +5,29 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+typedef struct Handler_format
+{
+	char c;
+	int (*f)(va_list args, flags_format *f);
+} switch_fromat;
+
+/* write */
+int _putchar(char c);
+int _puts(char *str);
+
+/* _printf */
 int _printf(const char *format, ...);
+
+/* get_print */
+int (*get_print(char s))(va_list, flags_format *);
+
+/* get_flag */
+int get_flag(char s, flags_format *f);
+
+/* percent */
+int print_percent(va_list args, flags_format *f);
+
+/* address */
+int print_address(va_list args, flags_format *f);
+
 #endif
